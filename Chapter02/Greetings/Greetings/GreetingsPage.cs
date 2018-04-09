dcbb954d5c3a56399c6b2450cfd9ff4d1094a03d
mduberstein@gmail.com <mduberstein@gmail.com>
2018-04-08 20:16:45 -0400

    Replaced GreetingsPage from Xaml to code

---
+++ Chapter02/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public partial class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+            switch(Device.RuntimePlatform){
+                case Device.iOS:
+                    Padding = new Thickness(0, 40, 0, 0);
+                    break;
+            }
+            //Obsolete since 2.3.4
+            //Device.OnPlatform(iOS: () =>
+            //{
+            //    Padding = new Thickness(0, 40, 0, 0);
+            //});
+        }
+    }
+}
+
